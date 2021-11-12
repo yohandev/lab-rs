b75104add1e89e47b1e7ccf8e4a33ce91ee0e0f9
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-11-12 00:48:23 -0500

    Tested & fixed simple lexer

--- rouille/src/lexer.rs
+++ rouille/src/lexer.rs
@@ -31,12 +31,14 @@ pub enum TokenKind
     RParen,     // )
 
     Fn,         // fn
+    Let,        // let
     Return,     // return
     
     Plus,       // +
     Minus,      // -
     Asterisk,   // *
     Slash,      // /
+    Equals,     // =
 
     Ident,      // Alphanumeric identifier
     Num,        // Integer or floating point
@@ -47,21 +49,23 @@ impl<'a> Tokens<'a>
     pub fn new(src: &'a str) -> Self
     {
         let pat = Box::new([
-            (TokenKind::Whitespace, Regex::new(r"^( +)").unwrap()),
-            (TokenKind::LBrace, Regex::new(r"^{").unwrap()),
-            (TokenKind::RBrace, Regex::new(r"^}").unwrap()),
+            (TokenKind::Whitespace, Regex::new(r"^([ \n\t\f]+)").unwrap()),
+            (TokenKind::LBrace, Regex::new(r"^\{").unwrap()),
+            (TokenKind::RBrace, Regex::new(r"^\}").unwrap()),
             (TokenKind::LParen, Regex::new(r"^\(").unwrap()),
             (TokenKind::RParen, Regex::new(r"^\)").unwrap()),
 
             (TokenKind::Fn, Regex::new(r"^fn").unwrap()),
+            (TokenKind::Let, Regex::new(r"^let").unwrap()),
             (TokenKind::Return, Regex::new(r"^return").unwrap()),
 
             (TokenKind::Plus, Regex::new(r"^\+").unwrap()),
             (TokenKind::Minus, Regex::new(r"^-").unwrap()),
             (TokenKind::Asterisk, Regex::new(r"^\*").unwrap()),
-            (TokenKind::Slash, Regex::new(r"^\/").unwrap()),
+            (TokenKind::Slash, Regex::new(r"^/").unwrap()),
+            (TokenKind::Equals, Regex::new(r"^=").unwrap()),
             
-            (TokenKind::Ident, Regex::new(r"^[A-Za-z][A-Za-z0-9]+").unwrap()),
+            (TokenKind::Ident, Regex::new(r"^[A-Za-z]+([A-Za-z0-9]+)?").unwrap()),
             (TokenKind::Num, Regex::new(r"^([0-9]+)(\.[0-9]+)?").unwrap()),
         ]);
 
@@ -81,28 +85,45 @@ impl<'a> Iterator for Tokens<'a>
             return None;
         }
 
-        let src = &self.src[self.next..];
-
         // Disambiguate tokens by order they're evaluated
         for (kind, pat) in &*self.pat
         {
-            // Pattern will only match start of line(via '\A')
-            let mat = pat.find(src);
+            // Pattern will only match start of line(via '^')
+            let mat = pat.find(&self.src[self.next..]);
 
             if let Some(mat) = mat
             {
                 // Next token
-                self.next = mat.end();
+                self.next += mat.end();
 
                 return Some(Ok(Token
                 {
                     kind: *kind,
-                    span: mat.range(),
+                    span: self.next - mat.end()..self.next,
                     text: mat.as_str(),
                 }));
             }
         }
         // Unknown token, TODO: skip unknown, continue with other tokens
+        self.next = self.src.len();
         Some(Err(()))
+    }
+}
+
+impl<'a> Token<'a>
+{
+    pub fn kind(&self) -> TokenKind
+    {
+        self.kind
+    }
+
+    pub fn span(&self) -> Range<usize>
+    {
+        self.span.clone()
+    }
+
+    pub fn as_str(&self) -> &'a str
+    {
+        self.text
     }
 }
---
+++ rouille/src/tests.rs
@@ -0,0 +1,104 @@
+use std::ops::Range;
+
+use crate::lexer::*;
+
+fn check(iter: &mut Tokens, kind: TokenKind, text: &str, span: impl Into<Option<Range<usize>>>)
+{
+    let token = iter
+        .next()
+        .unwrap()
+        .unwrap();
+
+    assert_eq!(token.kind(), kind);
+    assert_eq!(token.as_str(), text);
+    
+    // On some tests I'm too lazy to check the span
+    if let Some(span) = span.into()
+    {
+        assert_eq!(token.span(), span);
+    }
+    dbg!(token);
+}
+
+#[test]
+fn test_lexer_00()
+{
+    let mut tokens = Tokens::new("hello world");
+
+    check(&mut tokens, TokenKind::Ident, "hello", None);
+    check(&mut tokens, TokenKind::Whitespace, " ", None);
+    check(&mut tokens, TokenKind::Ident, "world", None);
+}
+
+#[test]
+fn test_lexer_01()
+{
+    let mut tokens = Tokens::new("x return world fn");
+
+    check(&mut tokens, TokenKind::Ident, "x", None);
+    check(&mut tokens, TokenKind::Whitespace, " ", None);
+    check(&mut tokens, TokenKind::Return, "return", None);
+    check(&mut tokens, TokenKind::Whitespace, " ", None);
+    check(&mut tokens, TokenKind::Ident, "world", None);
+    check(&mut tokens, TokenKind::Whitespace, " ", None);
+    check(&mut tokens, TokenKind::Fn, "fn", None);
+}
+
+#[test]
+fn test_lexer_02()
+{
+    let mut tokens = Tokens::new("let x = 42");
+
+    check(&mut tokens, TokenKind::Let, "let", None);
+    check(&mut tokens, TokenKind::Whitespace, " ", None);
+    check(&mut tokens, TokenKind::Ident, "x", None);
+    check(&mut tokens, TokenKind::Whitespace, " ", None);
+    check(&mut tokens, TokenKind::Equals, "=", None);
+    check(&mut tokens, TokenKind::Whitespace, " ", None);
+    check(&mut tokens, TokenKind::Num, "42", None);
+}
+
+#[test]
+fn test_lexer_03()
+{
+    let mut tokens = Tokens::new("
+    let x = 42
+    let y = 50.20
+    ");
+
+    check(&mut tokens, TokenKind::Whitespace, "\n    ", None);
+
+    check(&mut tokens, TokenKind::Let, "let", None);
+    check(&mut tokens, TokenKind::Whitespace, " ", None);
+    check(&mut tokens, TokenKind::Ident, "x", None);
+    check(&mut tokens, TokenKind::Whitespace, " ", None);
+    check(&mut tokens, TokenKind::Equals, "=", None);
+    check(&mut tokens, TokenKind::Whitespace, " ", None);
+    check(&mut tokens, TokenKind::Num, "42", None);
+
+    check(&mut tokens, TokenKind::Whitespace, "\n    ", None);
+
+    check(&mut tokens, TokenKind::Let, "let", None);
+    check(&mut tokens, TokenKind::Whitespace, " ", None);
+    check(&mut tokens, TokenKind::Ident, "y", None);
+    check(&mut tokens, TokenKind::Whitespace, " ", None);
+    check(&mut tokens, TokenKind::Equals, "=", None);
+    check(&mut tokens, TokenKind::Whitespace, " ", None);
+    check(&mut tokens, TokenKind::Num, "50.20", None);
+}
+
+#[test]
+fn test_lexer_04()
+{
+    let tokens = Tokens::new("let foo = (42 + 50) / 1");
+    let tokens = tokens
+        .map(|tk| tk.unwrap().as_str())
+        .collect::<Vec<_>>();
+    
+    assert_eq!(tokens, vec!
+    [
+        "let", " ", "foo", " ", "=",
+        " ", "(", "42", " ", "+", " ",
+        "50", ")", " ", "/", " ", "1"
+    ]);
+}
