1c59aed232832548a71dfd23ff28619f3d2d703f
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-11-24 21:51:04 -0500

    Added panic hook

--- photoshop/src/index.js
+++ photoshop/src/index.js
@@ -3,7 +3,20 @@ import mod from './lib.rs'
 
 (async () =>
 {
-    const { add } = await mod()
+    const { add, set_panic_hook, memory } = await mod({
+        env:
+        {
+            abort: (ptr, len) =>
+            {
+                // Reconstitute string
+                const buf = new Uint8Array(memory.buffer, ptr, len)
+                
+                alert(new TextDecoder('UTF-8').decode(buf))
+            }
+        }
+    })
+
+    set_panic_hook()
 
     m.render(document.body, <div>Hello, World! 3 + 4 = {add(3, 4)}</div>)
 })()
--- photoshop/src/lib.rs
+++ photoshop/src/lib.rs
@@ -1,5 +1,22 @@
+extern
+{
+    /// Kill execution of this WASM module with error message at `ptr`
+    fn abort(ptr: *const u8, len: usize);
+}
+
 #[no_mangle]
 pub fn add(a: i32, b: i32) -> i32
 {
     a + b
+}
+
+#[no_mangle]
+pub fn set_panic_hook()
+{
+    std::panic::set_hook(Box::new(|info| unsafe
+    {
+        let err = info.to_string();
+
+        abort(err.as_ptr(), err.len())
+    }))
 }
